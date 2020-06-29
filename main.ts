70fd1248b86587a12b82f0a9a4aa3fdfbaa04088
TPAMarvin <31587596+TPAMarvin@users.noreply.github.com>
2020-06-29 18:53:59 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    music.startMelody(music.builtInMelody(Melodies.Ringtone), MelodyOptions.Once)
+})
+input.onButtonPressed(Button.AB, function () {
+    music.startMelody(music.builtInMelody(Melodies.Birthday), MelodyOptions.Once)
+})
+input.onButtonPressed(Button.B, function () {
+    music.startMelody(music.builtInMelody(Melodies.JumpDown), MelodyOptions.Once)
+})
